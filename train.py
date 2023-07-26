a7a4456e47289c5f42610a802032bc1afbdcec32
dleston <140494264+dleston@users.noreply.github.com>
2023-07-26 10:37:11 +0000

    Checkpoint - initial submission

--- train.py
+++ train.py
@@ -68,5 +68,7 @@ def main():
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
+    joblib.dump(model, 'outputs/model.pkl')
+
 if __name__ == '__main__':
     main()
